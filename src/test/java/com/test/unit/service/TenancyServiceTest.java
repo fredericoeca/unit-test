812d795e62ee15d736e1bee790dea988e62756b7
fredericoeca <frederico-cabral@hotmail.com>
2020-12-29 12:06:54 -0300

    testing division

--- src/test/java/com/test/unit/service/TenancyServiceTest.java
+++ src/test/java/com/test/unit/service/TenancyServiceTest.java
@@ -1,8 +1,8 @@
 package com.test.unit.service;
 
-import static com.test.unit.util.DateUtils.getDateWithDifferenceOfTheDays;
-import static com.test.unit.util.DateUtils.isDateEquals;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static com.test.unit.util.DateUtils.*;
 
 import java.util.Date;
 
@@ -15,7 +15,7 @@ import com.test.unit.entity.User;
 public class TenancyServiceTest {
 
 	@Test
-	public void teste() {
+	public void testValue() {
 		
 		// scenario
 		TenancyService service = new TenancyService();
@@ -26,9 +26,37 @@ public class TenancyServiceTest {
 		Tenancy tenancy = service.rentMovie(user, movie);
 		
 		// checks
-		assertThat(tenancy.getValue().equals(5.0));
-		assertThat(isDateEquals(tenancy.getTenancyDate(), new Date()));
-		assertThat(isDateEquals(tenancy.getReturnDate(), getDateWithDifferenceOfTheDays(1)));
+		assertEquals(6.0, tenancy.getValue(), 0.01);
+	}
+	
+	@Test
+	public void testTenancyDate() {
+		
+		// scenario
+		TenancyService service = new TenancyService();
+		User user = new User("User One");
+		Movie movie = new Movie("Movie 1", 2, 5.0);		
+		
+		// action
+		Tenancy tenancy = service.rentMovie(user, movie);
+		
+		// checks
+		assertTrue(isDateEquals(tenancy.getTenancyDate(), new Date()));
+	}
+	
+	@Test
+	public void testReturnDate() {
+		
+		// scenario
+		TenancyService service = new TenancyService();
+		User user = new User("User One");
+		Movie movie = new Movie("Movie 1", 2, 5.0);		
+		
+		// action
+		Tenancy tenancy = service.rentMovie(user, movie);
+		
+		// checks
+		assertTrue(isDateEquals(tenancy.getReturnDate(), getDateWithDifferenceOfTheDays(2)));
 	}
 	
 }
