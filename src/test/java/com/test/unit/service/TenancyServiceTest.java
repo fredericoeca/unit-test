638a49e10ae45a1b85b89e3564e2370e6167bf52
fredericoeca <frederico-cabral@hotmail.com>
2020-12-29 12:13:26 -0300

    several tests with ErrorCollector

--- src/test/java/com/test/unit/service/TenancyServiceTest.java
+++ src/test/java/com/test/unit/service/TenancyServiceTest.java
@@ -1,12 +1,15 @@
 package com.test.unit.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static com.test.unit.util.DateUtils.*;
+import static com.test.unit.util.DateUtils.getDateWithDifferenceOfTheDays;
+import static com.test.unit.util.DateUtils.isDateEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import com.test.unit.entity.Movie;
 import com.test.unit.entity.Tenancy;
@@ -14,9 +17,12 @@ import com.test.unit.entity.User;
 
 public class TenancyServiceTest {
 
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void testValue() {
-		
+	public void testeTenancy() {
+				
 		// scenario
 		TenancyService service = new TenancyService();
 		User user = new User("User One");
@@ -26,37 +32,8 @@ public class TenancyServiceTest {
 		Tenancy tenancy = service.rentMovie(user, movie);
 		
 		// checks
-		assertEquals(6.0, tenancy.getValue(), 0.01);
+		error.checkThat(tenancy.getValue(), is(equalTo(6.0)));
+		error.checkThat(isDateEquals(tenancy.getTenancyDate(), new Date()), is(true));
+		error.checkThat(isDateEquals(tenancy.getReturnDate(), getDateWithDifferenceOfTheDays(1)), is(false));
 	}
-	
-	@Test
-	public void testTenancyDate() {
-		
-		// scenario
-		TenancyService service = new TenancyService();
-		User user = new User("User One");
-		Movie movie = new Movie("Movie 1", 2, 5.0);		
-		
-		// action
-		Tenancy tenancy = service.rentMovie(user, movie);
-		
-		// checks
-		assertTrue(isDateEquals(tenancy.getTenancyDate(), new Date()));
-	}
-	
-	@Test
-	public void testReturnDate() {
-		
-		// scenario
-		TenancyService service = new TenancyService();
-		User user = new User("User One");
-		Movie movie = new Movie("Movie 1", 2, 5.0);		
-		
-		// action
-		Tenancy tenancy = service.rentMovie(user, movie);
-		
-		// checks
-		assertTrue(isDateEquals(tenancy.getReturnDate(), getDateWithDifferenceOfTheDays(2)));
-	}
-	
 }
